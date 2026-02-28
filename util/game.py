c1b6bd1bc543b2d83ad53891d11c71d81d3a0aac
Mimmi Tamminen <mimmi.tamminen@tuni.fi>
2026-02-28 23:43:43 +0200

    game.py(run_game): Add basic reading for user input, for testing game loop

--- util/game.py
+++ util/game.py
@@ -5,5 +5,11 @@ class Game:
     def __init__(self):
         self.running = True
 
+# TODO: Build basic run of the game here
     def run_game(self):
-        pass
+        user_input = input("\n> ")      # read user input
+
+        if user_input.lower() == "stop":
+            self.running = False        # stop game
+        else:
+            print(user_input.upper())
