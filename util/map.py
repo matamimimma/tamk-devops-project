b3d05f2fbc880da2740553c11d9f5488cea81bcf
Mimmi Metropolia <mimmitam@metropolia.fi>
2026-03-01 13:26:33 +0200

    map.py(__init__): Add prototype for map structure

--- util/map.py
+++ util/map.py
@@ -3,4 +3,21 @@
 
 class Map:
     def __init__(self):
-        self.areas = []
+        self.areas = [
+            {
+                "name": "area1"
+                , "desc": "Area1 description."
+                , "move": [None, None, None, None]
+                        # [north, east, south, west]
+            },
+            {
+                "name": "area2"
+                , "desc": "Area2 description."
+                , "move": [None, None, None, None]
+            },
+            {
+                "name": "area2"
+                , "desc": "Area2 description."
+                , "move": [None, None, None, None]
+            }
+        ]
