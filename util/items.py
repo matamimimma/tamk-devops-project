6d0c977befd86d83902755e1877858695d487ac4
Mimmi Metropolia <mimmitam@metropolia.fi>
2026-03-02 22:06:41 +0200

    items.py(__init__): Add prototype for item list

--- util/items.py
+++ util/items.py
@@ -3,4 +3,20 @@
 
 class Items:
     def __init__(self):
-        self.all = []
+        self.all = {
+            "item1": {
+                "desc": "Item1 description"
+                , "action": None
+                , "action_desc": None
+            },
+            "item2": {
+                "desc": "Item2 description"
+                , "action": None
+                , "action_desc": None
+            },
+            "item3": {
+                "desc": "Item3 description"
+                , "action": None
+                , "action_desc": None
+            }
+        }
