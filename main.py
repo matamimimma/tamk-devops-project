380f28ce31bc5f8b1bc67bfdc6b47034156f26ff
Mimmi Tamminen <mimmi.tamminen@tuni.fi>
2026-03-02 15:22:30 +0200

    main.py: Initialize Map and Player

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from util.map import Map
 from util.player import Player
 
 if __name__ == "__main__":
+
+    start_pos = 0
+
+    map = Map()
+    player = Player(start_pos)
     game = Game()
 
     print("-- START --")
