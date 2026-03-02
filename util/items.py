fb43e3291126da815e46718bd43c0eb5565eaa77
Mimmi Metropolia <mimmitam@metropolia.fi>
2026-03-02 22:22:38 +0200

    items.py: Add testing for code

--- util/items.py
+++ util/items.py
@@ -24,3 +24,12 @@ class Items:
 # Print items description
     def print_item_description(self, item):
         print(f"{item:<15} - {self.all[item]['desc']}")
+
+# TEST code here
+if __name__ == "__main__":
+    items = Items()
+    inv = ["item1", "item3"]
+
+    for item in inv:
+        if item in items.all:
+            items.print_item_description(item)
