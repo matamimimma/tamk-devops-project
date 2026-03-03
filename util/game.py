211326ed3afe0147887df3256f6ad69b65dfd118
Mimmi Metropolia <mimmitam@metropolia.fi>
2026-03-03 23:46:18 +0200

    game.py(__init__): Add 'investigate' to used game commands

--- util/game.py
+++ util/game.py
@@ -4,7 +4,7 @@
 class Game:
     def __init__(self, map, player):
         self.running = True
-        self.commands = ["stop", "go", "take", "drop", "inventory"]
+        self.commands = ["stop", "go", "take", "drop", "inventory", "investigate"]
         self.map = map
         self.player = player
 
