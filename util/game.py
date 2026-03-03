9bc477aace5ef20e60fbebe13112183549585417
Mimmi Metropolia <mimmitam@metropolia.fi>
2026-03-03 23:39:55 +0200

    game.py(__init__): Add 'inventory' to used game commands

--- util/game.py
+++ util/game.py
@@ -4,7 +4,7 @@
 class Game:
     def __init__(self, map, player):
         self.running = True
-        self.commands = ["stop", "go", "take", "drop"]
+        self.commands = ["stop", "go", "take", "drop", "inventory"]
         self.map = map
         self.player = player
 
